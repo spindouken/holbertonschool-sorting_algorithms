06ebe28a5ae595a2777ae2351a98824a7e4cbf81
spindouken <spindouken@gmail.com>
2022-12-16 15:34:55 -0600

    local push

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,11 +10,10 @@
 void bubble_sort(int *array, size_t size)
 {
 	int temp;
-	size_t i, j;
 
-	for (i = 0; i < size - 1; i++)
+	for (size_t i = 0; i < size - 1; i++)
 	{
-		for (j = 0; j < size - i - 1; j++)
+		for (size_t j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,35 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list - sorts a doubly linked list of 
+ * integers in ascending order using insertion sort
+ * @list: list
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *current = NULL;
+
+	if (list == NULL || *list == NULL || (*list)->next == NULL)
+		return;
+
+	current = (*list)->next;
+	while (current != NULL)
+	{
+		while (current->prev != NULL && current->prev->n > current->n)
+		{
+			current->prev->next = current->next;
+			if (current->next != NULL)
+				current->next->prev = current->prev;
+			current->next = current->prev;
+			current->prev = current->next->prev;
+			current->next->prev = current;
+			if (current->prev == NULL)
+				*list = current;
+			else
+				current->prev->next = current;
+			print_list(*list);
+		}
+		current = current->next;
+	}
+}
