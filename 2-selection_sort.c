04a210ff397492b4ac1ad76c8487a58f4dc8c4d4
spindouken <spindouken@gmail.com>
2022-12-16 15:51:11 -0600

    2 finished with printf implementation

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of integers in 
+ * ascending order using the selection sort algorithm
+ * @array: array
+ * @size: size of the array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j, min;
+	int temp;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = i;
+		for (j = i + 1; j < size; j++)
+		{
+			if (array[j] < array[min])
+				min = j;
+		}
+		if (min != i)
+		{
+			printf("Swapping %d with %d\n", array[i], array[min]);
+			temp = array[i];
+			array[i] = array[min];
+			array[min] = temp;
+			print_array(array, size);
+		}
+	}
+}
