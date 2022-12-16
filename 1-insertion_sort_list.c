055916a46c39dd8572d3f8d749481a3506467d02
spindouken <spindouken@gmail.com>
2022-12-16 15:58:46 -0600

    betty compliance

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -1,7 +1,7 @@
 #include "sort.h"
 
 /**
- * insertion_sort_list - sorts a doubly linked list of 
+ * insertion_sort_list - sorts a doubly linked list of
  * integers in ascending order using insertion sort
  * @list: list
  */
