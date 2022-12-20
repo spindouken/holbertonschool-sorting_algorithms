1c70560d42055b69f2dc7326f68230eb333d40e0
spindouken <spindouken@gmail.com>
2022-12-19 22:33:58 -0600

    edge hunt

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,8 @@ void bubble_sort(int *array, size_t size)
 	int temp;
 	size_t i, j;
 
+	if (array == NULL || size < 2)
+		return;
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
